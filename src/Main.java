6ef6d2676b38768816377200cc8715ac4b43ba79
N1ka2008 <turcsanyiova@spsejecna.cy>
2025-03-04 15:36:06 +0100

    Pridani vypisu moznich mistnosti a uprava v lokaci

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Lokace lokace = new Lokace(".idea/soub.txt");
         try {
             System.out.println("Mistnosti v 1. patre: hala, koupelna, kuchyn, jidelna, schodiste");
             System.out.println("Mistnosti ve 2. patre: chodba, detsky pokoj, 2. detsky pokoj, loznice, schodiste");
             System.out.println("Mistnosti ve sklepe: sklep, schodiste");
-            Lokace lokace = new Lokace(".idea/soub.txt");
+            //Lokace lokace = new Lokace(".idea/soub.txt");
             Pohyb po = new Pohyb(lokace.getMistnost("hala"));
 
             Scanner sc = new Scanner(System.in);
@@ -17,9 +18,10 @@ public class Main {
                 String cil = sc.nextLine();
                 po.jit(cil);
             }
-        } catch (IOException e) {
+        } /*catch (IOException e) {
             System.out.println("Chyba při načítání souboru: ");
-        } catch (Exception e) {
+            */
+        catch (Exception e) {
             System.out.println(e);
         }
     }
