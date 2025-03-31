fe0c2df2cf37e8dc194722142832925377ccefe0
N1ka2008 <turcsanyiova@spsejecna.cy>
2025-03-31 15:28:41 +0200

    Upravy v main

---
+++ src/PohybTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.IOException;
+public class PohybTest {
+
+    private Lokace lokace;
+    private Pohyb pohyb;
+
+    @Before
+    public void setUp() throws IOException {
+        lokace = new Lokace(".idea/soub.txt");
+        pohyb = new Pohyb(lokace.getMistnost("hala"));
+    }
+
+    @Test
+    public void testPohybMeziMistnostmi() {
+
+        String pocatecniMistnost = pohyb.getAktualniMistnost();
+        assertEquals("hala", pocatecniMistnost);
+
+        pohyb.jit("koupelna");
+        assertEquals("koupelna", pohyb.getAktualniMistnost());
+
+        String predPresun = pohyb.getAktualniMistnost();
+        pohyb.jit("sklep");
+        assertEquals(predPresun, pohyb.getAktualniMistnost());
+    }
+}
